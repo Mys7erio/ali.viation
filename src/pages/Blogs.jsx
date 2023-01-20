0bb79001d8dc2714f191a1434b582aec4b3ba6cb
Shakir <shakirali.sacube@gmail.com>
2023-01-20 19:43:22 +0530

    Added blogs section & badges now have a background color

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,7 +1,15 @@
 export default function Blogs() {
+    const pageStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily: 'Comfortaa'
+    }
     return(
-        <div className="page">
-            <h1>Blogs</h1>
+        <div className="page" style={pageStyle}>
+            <h1>Hang on tight, I'm still working on the blogs</h1>
+            <h1 style={{fontSize: '10vh'}}>;)</h1>
         </div>
     )
 }
